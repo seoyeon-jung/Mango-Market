6f32d38c4557d3ea21e8f99ac8c2257a67834c8b
Jinsu <jjs0517@naver.com>
2023-01-10 17:43:53 +0900

    home 화면 기초적인 기능구현#6

--- firebase.js
+++ firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from "firebase/auth"; // 아래 데이터는 본인의 Fireba
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyA7urirbxoXBsC8h7DHN1LDxa1IDoei4MM",
+  authDomain: "fir-dd60a.firebaseapp.com",
+  projectId: "fir-dd60a",
+  storageBucket: "fir-dd60a.appspot.com",
+  messagingSenderId: "487503040464",
+  appId: "1:487503040464:web:c2de2dc22f763be3bbb3d5",
 };
 
-export const storage = getStorage(app);
 export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
 export const dbService = getFirestore(app);
 export const authService = getAuth(app);
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,12 +1,48 @@
 import { useFocusEffect } from "@react-navigation/native";
+import Post from "../components/Post";
 import { signOut } from "firebase/auth";
 import React, { useCallback } from "react";
-import { View, Text, TouchableOpacity, useColorScheme } from "react-native";
-import { authService } from "../firebase";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  ScrollView,
+  Image,
+  FlatList,
+} from "react-native";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { authService, dbService } from "../firebase.js";
+import styled from "@emotion/native";
+import { SCREEN_HEIGHT } from "../util";
 
 export default function Home({ navigation: { navigate, reset } }) {
-  const isDark = useColorScheme() === "dark";
+  const [posts, setPosts] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const getPostDate = () => {
+    const q = query(
+      collection(dbService, "posts"),
+      orderBy("date"),
+      where("isDone", "==", false)
+    );
 
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const newPosts = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setPosts(newPosts);
+    });
+    return unsubscribe;
+  };
   useFocusEffect(
     useCallback(() => {
       if (!authService.currentUser) {
@@ -30,18 +66,55 @@ export default function Home({ navigation: { navigate, reset } }) {
         });
         return;
       }
-    })
+      getPostDate();
+    }, [])
   );
+  //새로고침
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+    await getPostDate();
+    setIsRefreshing(false);
+  };
 
   return (
-    <View>
-      <TouchableOpacity
-        onPress={() => navigate("Stacks", { screen: "Detail" })}
-      >
-        <Text style={{ color: isDark ? "white" : "black" }}>
-          this is home(if click, you can go to detail page)
-        </Text>
-      </TouchableOpacity>
-    </View>
+    <FlatList
+      style={{ backgroundColor: "white" }}
+      refreshing={isRefreshing}
+      onRefresh={onRefresh}
+      data={posts}
+      renderItem={({ item }) => <Post item={item} />}
+      keyExtractor={(item) => item.id}
+      ListHeaderComponent={
+        <Header>
+          <Image
+            style={{ width: 40, height: 40 }}
+            source={{
+              uri: "https://i.ibb.co/gvpPs61/image.png",
+            }}
+          />
+          <TitleText>망고마켓</TitleText>
+        </Header>
+      }
+    />
   );
 }
+const Header = styled.View`
+  margin-right: 30px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex:1
+  height:${SCREEN_HEIGHT / 8 + "px"};
+
+
+`;
+const TitleText = styled.Text`
+  text-shadow: 1px 2px 2px;
+  font-size: 40px;
+  font-weight: 5;
+  color: #f4cd43;
+`;
+const ConTainer = styled.View`
+  height: ${SCREEN_HEIGHT + "px"};
+  background-color: white;
+`;
