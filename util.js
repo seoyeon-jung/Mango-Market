8daad49dd31ff86073c7cfd2033398b9451e5d8e
jaehyun2yo <68274491+jaehyun2yo@users.noreply.github.com>
2023-01-13 10:37:17 +0900

    develop -> master

--- util.js
+++ util.js
@@ -7,5 +7,3 @@ export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } =
 export const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 export const pwRegex =
   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
-
-  // 시작
